bb603dd566a5ae2814a5a140652230054c04574b
Sibahle Mayekiso <sibahle.mayekiso@gamesglobal.com>
2023-08-16 08:51:42 +0000

    MR: Renamed Model Properties, Formatted Files and Restructured Models

--- BlueFoxGloveAPI/Models/Game.cs
+++ BlueFoxGloveAPI/Models/Game.cs
@@ -6,25 +6,13 @@ namespace BlueFoxGloveAPI.Models
     public class Game
     {
         [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public ObjectId Id { get; set; }
-        [BsonRepresentation(BsonType.ObjectId)]
-        public ObjectId PlayerId { get; set; }
-        [BsonElement("gamename")]
-        public string GameName { get; set; }
-        [BsonElement("xco-ordinate")]
-        public int XCoordinate { get; set; }
-        [BsonElement("yco-ordinate")]
-        public int YCoordinate { get; set; }
-        [BsonElement("playercolor")]
-        public string PlayerColor { get; set; }
-        [BsonElement("points")]
-        public int Points { get; set; }
-        [BsonElement("timestamp")]
-        public DateTime GameTimeStamp { get; set; }
-        [BsonElement("health")]
-        public int Health { get; set; }
+        [BsonElement("gamesessionid")]
+        public string GameSessionId { get; set; }
 
+        [BsonElement("gamesessiontimestamp")]
+        public DateTime GameSessionTimeStamp { get; set; }
 
+        [BsonElement("playersjoiningsession")]
+        public List<Player> PlayersJoiningSession { get; set; }
     }
 }
