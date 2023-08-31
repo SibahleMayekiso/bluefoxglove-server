daabf564a5226343a2060cc9b852ff9a0b84913c
Sibahle <mayekisosibahle@gmail.com>
2023-08-31 06:26:25 +0000

    Resolve "A Player Loads Their Profile And Is Shown With Their Available Stats"

---
+++ BlueFoxGloveAPI.Tests/PlayerProfileRepositoryTest.cs
@@ -0,0 +1,65 @@
+﻿using BlueFoxGloveAPI.Models;
+using BlueFoxGloveAPI.Repository.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BlueFoxGloveAPI.Tests
+{
+    [TestFixture]
+    public class PlayerProfileRepositoryTest
+    {
+        private IPlayerProfileRepository _playerProfileRepository;
+        private List<PlayerProfile> _playerProfiles;
+
+        [SetUp]
+        public void Setup()
+        {
+            _playerProfileRepository = Substitute.For<IPlayerProfileRepository>();
+            _playerProfiles = new List<PlayerProfile> {
+                new PlayerProfile {
+                    PlayerId = "64dd1cf27a6922a9502fc8be",
+                    PlayerName = "JohnDoe001",
+                    LongestSurvivalTime = 0,
+                    TotalPlayTime = 0,
+                    NumberOfGamesPlayed = 0,
+                    KillCount = 0
+                }
+            };
+        }
+
+        [Test]
+        public void GetPlayerProfileById_WhenCalledWithAValidPlayerId_ReturnPlayerProfile()
+        {
+            //Arrange
+            var playerProfile = _playerProfiles[0];
+            var playerId = playerProfile.PlayerId ?? "";
+            var expected = new List<PlayerProfile> {
+                playerProfile
+            };
+
+            _playerProfileRepository.GetPlayerProfileById(playerId).Returns(expected);
+
+            //Action
+            var actual = _playerProfileRepository.GetPlayerProfileById(playerId).Result;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void GetPlayerProfileById_WhenCalledWithNonExistantPlayerId_ReturnEmptyList()
+        {
+            //Arrange
+            var playerId = "64dd1cf27a6922a9502fc8be";
+            var expected = new List<PlayerProfile>();
+
+            _playerProfileRepository.GetPlayerProfileById(playerId).Returns(new List<PlayerProfile>());
+
+            //Action
+            var actual = _playerProfileRepository.GetPlayerProfileById(playerId).Result;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ BlueFoxGloveAPI/Controllers/PlayerProfileController.cs
@@ -0,0 +1,30 @@
+﻿using BlueFoxGloveAPI.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlueFoxGloveAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PlayerProfileController : ControllerBase
+    {
+        private readonly IPlayerProfileRepository _playerProfileRepository;
+
+        public PlayerProfileController(IPlayerProfileRepository playerProfileRepository)
+        {
+            _playerProfileRepository = playerProfileRepository;
+        }
+
+        [HttpGet("/playerprofile/{playerId}")]
+        public async Task<IActionResult> GetPlayerProfileById(string playerId)
+        {
+            var playerProfile = await _playerProfileRepository.GetPlayerProfileById(playerId);
+
+            if (playerProfile.Count == 0)
+            {
+                return NotFound(playerProfile);
+            }
+
+            return Ok(playerProfile);
+        }
+    }
+}
